d3bff4c28a135a1e3443575d2a879d07d0042f49
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-02-08 23:56:06 +0300

    Implement ControlTask.cs file

--- ControlTask.cs
+++ ControlTask.cs
@@ -6,7 +6,19 @@ namespace func_rocket
 	{
 		public static Turn ControlRocket(Rocket rocket, Vector target)
 		{
-			return Turn.None;
+			double targetAngle = Math.Atan2((target - rocket.Location).Y, (target - rocket.Location).X);
+			double rocketDirectionAngle = Math.Atan2(Math.Sin(rocket.Direction), Math.Cos(rocket.Direction));
+			double velocityAngle = rocket.Velocity.Angle;
+			double difBetweenTargetAndVelocity = DifferenceBetweenAngles(targetAngle, velocityAngle);
+			double idealRocketDirectionAngle;
+			if (Math.Abs(difBetweenTargetAndVelocity) < Math.PI / 4)
+				idealRocketDirectionAngle = targetAngle + difBetweenTargetAndVelocity;
+			else
+				idealRocketDirectionAngle = targetAngle + Math.PI / 8;
+			idealRocketDirectionAngle = Math.Atan2(Math.Sin(idealRocketDirectionAngle), Math.Cos(idealRocketDirectionAngle)); // Здесь необходим угол без дополнительных кругов.
+			double difBetweenIdealAndRealRocketDir = DifferenceBetweenAngles(idealRocketDirectionAngle, rocketDirectionAngle);
+			return difBetweenIdealAndRealRocketDir > 0 ? Turn.Right : difBetweenIdealAndRealRocketDir < 0 ? Turn.Left : Turn.None;
 		}
+		static double DifferenceBetweenAngles(double first, double second) => (((first - second) + Math.PI) % (2 * Math.PI)) - Math.PI;
 	}
 }
